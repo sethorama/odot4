dc6fdf05f2093bdb6b819b9addce6cec1b26f6a5
Seth Reece <sethorama@bassplayer.st>
2015-05-04 11:09:28 -0700

    Fix pending features in todo list editing integration test

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -9,12 +9,11 @@ describe "Editing todo lists" do
     todo_list = options[:todo_list]
 
     visit "/todo_lists"
-    within "#todo_list_#{todo_list.id}" do
-      click_link "Edit"
-    end
+    click_link todo_list.title
+    click_link "Edit"
 
     fill_in "Title", with: options[:title]
-    click_button "Update Todo list"
+    click_button "Save"
   end
 
   before do
@@ -23,7 +22,6 @@ describe "Editing todo lists" do
 
 
   it "updates a todo list successfully with correct information" do
-    pending "Editing todo lists"
     update_todo_list todo_list: todo_list, 
                      title: "New title"
 
@@ -34,18 +32,16 @@ describe "Editing todo lists" do
   end
 
   it "displays an error with no title" do
-    pending "Editing todo lists"
     update_todo_list todo_list: todo_list, title: ""
     title = todo_list.title
     todo_list.reload
     expect(todo_list.title).to eq(title)
-    expect(page).to have_content("error")
+    expect(page).to have_content(/can't be blank/i)
   end
 
   it "displays an error with too short a title" do
-    pending "Editing todo lists"
     update_todo_list todo_list: todo_list, title: "hi"
-    expect(page).to have_content("error")
+    expect(page).to have_content(/is too short/i)
   end
 
 end
